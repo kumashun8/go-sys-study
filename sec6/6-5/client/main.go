6bf7825134f612421c7e24e8b187586fd1d0a7f4
kumashun8 <tiwo3wa4@gmail.com>
2023-01-04 23:22:58 +0900

    6-6 速度改善(1) HTTP/1.1のKeep-Aliveに対応させる

--- sec6/6-5/client/main.go
+++ sec6/6-5/client/main.go
@@ -6,27 +6,51 @@ import (
 	"net"
 	"net/http"
 	"net/http/httputil"
+	"strings"
 )
 
 const ServerAddr = "localhost:8888"
 
 func main() {
-	conn, err := net.Dial("tcp", ServerAddr)
-	if err != nil {
-		panic(err)
+	sendMessages := []string{
+		"ASCII",
+		"PROGRAMMING",
+		"PLUS",
 	}
-	request, err := http.NewRequest("GET", "http://"+ServerAddr, nil)
-	if err != nil {
-		panic(err)
+	current := 0
+	var conn net.Conn = nil
+	for {
+		var err error
+		if conn == nil {
+			conn, err = net.Dial("tcp", ServerAddr)
+			if err != nil {
+				panic(err)
+			}
+			fmt.Printf("Access: %d\n", current)
+		}
+		request, err := http.NewRequest(
+			"POST",
+			"http://"+ServerAddr,
+			strings.NewReader(sendMessages[current]))
+		if err != nil {
+			panic(err)
+		}
+		request.Write(conn)
+		response, err := http.ReadResponse(bufio.NewReader(conn), request)
+		if err != nil {
+			fmt.Println("Retry")
+			conn = nil
+			continue
+		}
+		dump, err := httputil.DumpResponse(response, true)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println(string(dump))
+		current++
+		if current == len(sendMessages) {
+			break
+		}
 	}
-	request.Write(conn)
-	response, err := http.ReadResponse(bufio.NewReader(conn), request)
-	if err != nil {
-		panic(err)
-	}
-	dump, err := httputil.DumpResponse(response, true)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println(string(dump))
+	conn.Close()
 }
