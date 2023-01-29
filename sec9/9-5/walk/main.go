994d7ca8a6a46c501987ef71d4f3c21726996abf
kumashun8 <tiwo3wa4@gmail.com>
2023-01-29 16:15:57 +0900

    9-5 path/filepathパッケージの関数たち

---
+++ sec9/9-5/walk/main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+var ImageSuffix = map[string]bool{
+	".jpg":  true,
+	".jpeg": true,
+	".png":  true,
+	".webp": true,
+	".gif":  true,
+	".tiff": true,
+	".eps":  true,
+}
+
+func main() {
+	if len(os.Args) == 1 {
+		fmt.Printf(`Find images
+Usage:
+    %s [path to find]`, os.Args[0])
+		return
+	}
+	root := os.Args[1]
+
+	fmt.Println("Images in", os.Args[1])
+	err := filepath.Walk(root,
+		func(path string, info os.FileInfo, err error) error {
+			if os.IsNotExist(err) {
+				fmt.Println("path name is wrong:", os.Args[1])
+				return nil
+			}
+			if info.IsDir() {
+				if info.Name() == "_build" {
+					return filepath.SkipDir
+				}
+				return nil
+			}
+			ext := strings.ToLower(filepath.Ext(info.Name()))
+			if ImageSuffix[ext] {
+				rel, err := filepath.Rel(root, path)
+				if err != nil {
+					return nil
+				}
+				fmt.Printf("%s\n", rel)
+			}
+			return nil
+		})
+	if err != nil {
+		fmt.Println(1, err)
+	}
+}
